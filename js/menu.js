ee18c151624ab0cd42c2d8064ba1f7562fae0432
Justin Duong <jduong@umich.edu>
2022-10-07 16:49:39 -0400

    Added css styles for 1200px and greater.

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const btnClose = document.getElementById('btn-close');
   const listMenu = document.querySelector('.list-menu');
   const bgOverlay = document.getElementById('bg-overlay');
+  const headerBtnRequest = document.querySelector('header > .btn-request');
+
+  if (window.innerWidth >= 922) {
+    btnHamburger.classList.add('hide');
+    btnClose.classList.add('hide');
+    listMenu.classList.remove('hide');
+    bgOverlay.classList.add('hide');
+    headerBtnRequest.classList.remove('hide');
+  } else {
+    btnHamburger.classList.remove('hide');
+    btnClose.classList.add('hide');
+    listMenu.classList.add('hide');
+    bgOverlay.classList.add('hide');
+    headerBtnRequest.classList.add('hide');
+  }
 
   btnHamburger.addEventListener('click', (e) => {
     e.preventDefault();
@@ -20,5 +35,21 @@ document.addEventListener('DOMContentLoaded', () => {
     btnClose.classList.add('hide');
     listMenu.classList.add('hide');
     bgOverlay.classList.add('hide');
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 922) {
+      btnHamburger.classList.add('hide');
+      btnClose.classList.add('hide');
+      listMenu.classList.remove('hide');
+      bgOverlay.classList.add('hide');
+      headerBtnRequest.classList.remove('hide');
+    } else {
+      btnHamburger.classList.remove('hide');
+      btnClose.classList.add('hide');
+      listMenu.classList.add('hide');
+      bgOverlay.classList.add('hide');
+      headerBtnRequest.classList.add('hide');
+    }
   });
 });
